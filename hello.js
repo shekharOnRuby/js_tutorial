dff66ec71dca131f156a5cc91a9d202331750d17
Shekhar Karande <shekhar.karande@gmail.com>
2018-04-27 14:32:01 +0100

    adding stuff  next 2.6

---
+++ hello.js
@@ -0,0 +1,13 @@
+console.log("hello,world!")
+
+let first_name = 'Shekhar'
+let last_name = 'Karande'
+
+console.log(first_name + " " + last_name)
+
+let city = 'London'
+let country = 'United Kingdom'
+
+console.log(`${first_name} lives in ${city}`)
+
+//let city = 'London'
