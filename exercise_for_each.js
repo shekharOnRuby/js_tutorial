80adda7ba41ebfb08dab3f84f0c96d1fe1ad8c3f
Shekhar Karande <shekhar.karande@gmail.com>
2018-05-10 16:53:27 +0100

    adding functions

---
+++ exercise_for_each.js
@@ -0,0 +1,9 @@
+let a = [8, 17, 42, 99];
+// > a.sort(function(a, b) { return a - b; });
+// [ 8, 17, 42, 99 ]
+
+a.forEach((a,b) => {
+    return (a-b)
+})
+
+console.log(a)
---
+++ forEach.js
@@ -0,0 +1,20 @@
+
+let a = ["ant", "bat", "cat", 47]
+
+// a.forEach(function(element){
+//     console.log(element)
+// })
+
+let soliloquy = " To be or not to be, that is the question: "
+
+// Array.from(soliloquy).forEach(function(element){
+//     console.log(element)
+// })
+
+a.forEach(element => {
+    console.log(element)
+})
+
+// Array.from(soliloquy).forEach(element => {
+//     console.log(element)
+// })
