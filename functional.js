57912edbef8f6c8066f4496d7c954108fae7bb38
shekhar karande <shekhar.karande@gmail.com>
2018-05-10 21:05:47 +0100

    adding exercise for map

--- functional.js
+++ functional.js
@@ -1,17 +1,24 @@
 let states = ["Kansas", "Nebraska", 'North Dakota', "South Dakota"]
+function urlify(string){
+    return string.toLowerCase().split(/\s+/).join('-')
+}
 
+function returnURL(states){
+    return  states.map(state => "http://example.com/"+ urlify(state))
+}
 function imperativeMap(states){
     let urlStates = []
     states.forEach(state => {
-        urlStates.push(state.toLowerCase().split(/\s+/).join("-"))
+        urlStates.push(urlify(state))
     });
     return urlStates
 }
 
 function functionalMap(states){
-return states.map((state => state.toLowerCase().split(/\s+/).join('-')))
+return states.map(state => urlify(state))
 }
 
 
 console.log(imperativeMap(states))
 console.log(functionalMap(states))
+console.log(returnURL(states))
