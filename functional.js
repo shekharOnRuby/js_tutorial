289fae3b1adfc4fa54b143bc5d92ddbe68f81a8c
Shekhar Karande <shekhar.karande@gmail.com>
2018-05-16 13:00:46 +0100

    finishing filter exercises

--- functional.js
+++ functional.js
@@ -19,6 +19,33 @@ return states.map(state => urlify(state))
 }
 
 
-console.log(imperativeMap(states))
-console.log(functionalMap(states))
-console.log(returnURL(states))
+// console.log(imperativeMap(states))
+// console.log(functionalMap(states))
+// console.log(returnURL(states))
+
+function imperativeFilter(states){
+    let singleWordStates = []
+    states.forEach(function(state){
+        if (state.split(/\s+/).length === 1){
+            singleWordStates.push(state)
+        }
+    })
+    return singleWordStates
+}
+
+function functionalFilter(states){
+    return states.filter(state => state.split(/\s+/).length ===1)
+}
+
+function functionalFilter2(states){
+    return states.filter(state => state.split(/\s+/).length ===2)
+}
+
+function functionalIncludes(states){
+    return states.filter(state => state.toLowerCase().includes("dakota"))
+}
+
+console.log(imperativeFilter(states))
+console.log(functionalFilter(states))
+console.log(functionalFilter2(states))
+console.log(functionalIncludes(states))
