4ebf10d60ad605ade5331bbf867e1f26164f1f75
Shekhar Karande <shekhar.karande@gmail.com>
2018-05-22 12:42:22 +0100

    adding the prototype translated phrase

--- palindrome.js
+++ palindrome.js
@@ -2,17 +2,30 @@ function reverse(str){
  return Array.from(str).reverse().join("")
 }
 
-
-
-
 function Phrase(content){
     this.content = content
+
+    this.processedContent = function processedContent() {
+        return this.content.toLowerCase()
+    }
+
     this.palindrome = function palindrome(){
-        let processedContent = this.content.toLowerCase()
-        return processedContent === reverse(processedContent)
+        
+        return this.processedContent() === reverse(this.processedContent())
     }
 
     this.louder = function louder(){
         return this.content.toUpperCase()
     }
 }
+
+function TranslatedPhrase(content, translation){
+    this.content = content;
+    this.translation = translation
+
+    this.processedContent = function processedContent(){
+        return this.translation.toLowerCase()
+    }
+}
+
+TranslatedPhrase.prototype = new Phrase()
