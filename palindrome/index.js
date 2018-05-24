fe1db58e08c145c1a2b791c3cd0494fd1f0a702e
Shekhar Karande <shekhar.karande@gmail.com>
2018-05-24 14:40:53 +0100

    red green refactor

---
+++ palindrome/index.js
@@ -0,0 +1,42 @@
+String.prototype.reverse = function(){
+ return Array.from(this).reverse().join("")
+}
+
+String.prototype.blankString = function(){
+    return this.match(/^\s$/)
+}
+function Phrase(content){
+    this.content = content
+
+    this.processor = function(string){
+        return string.toLowerCase()
+    }
+
+    this.processedContent = function processedContent() {
+        return this.processor(this.content)
+    }
+
+    this.palindrome = function palindrome(){
+        
+        return this.processedContent() === this.processedContent().reverse()
+    }
+
+    this.louder = function louder(){
+        return this.content.toUpperCase()
+    }
+
+    this.blank = function blank(){
+        return this.blankString()
+    }
+}
+
+function TranslatedPhrase(content, translation){
+    this.content = content;
+    this.translation = translation
+
+    this.processedContent = function processedContent(){
+        return this.processor(this.translation)
+    }
+}
+
+TranslatedPhrase.prototype = new Phrase()
