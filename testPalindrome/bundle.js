47dbebe6807063a4dcdfb133b1286cace3745244
shekhar karande <shekhar.karande@gmail.com>
2018-06-13 12:09:06 +0100

    adding event listeners and stuff

---
+++ testPalindrome/bundle.js
@@ -0,0 +1,61 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+let Phrase = require("shk-palindrome")
+
+let string = prompt("Please enter a string for palindrome testing: ")
+
+let phrase = new Phrase(string)
+
+if (phrase.palindrome()){
+    alert(`"${phrase.content}" is a Palindrome`)
+}else {
+    alert(`"${phrase.content}" is not a Palindrome`)
+}
+},{"shk-palindrome":2}],2:[function(require,module,exports){
+module.exports = Phrase
+
+String.prototype.reverse = function(){
+ return Array.from(this).reverse().join("")
+}
+
+String.prototype.blankString = function(){
+    return this.match(/^\s$/)
+}
+function Phrase(content){
+    this.content = content
+
+    this.processor = function(string){
+        return string.toLowerCase()
+    }
+
+    this.letters = function letters(){
+        return Array.from(this.content).filter(c => c.match(/[a-z]/i)).join("")
+    }
+    
+    this.processedContent = function processedContent() {
+        return this.processor(this.content)
+    }
+
+    this.palindrome = function palindrome(){
+        return this.processedContent() === this.processedContent().reverse()
+    }
+
+    this.louder = function louder(){
+        return this.content.toUpperCase()
+    }
+
+    this.blank = function blank(){
+        return this.blankString()
+    }
+}
+
+function TranslatedPhrase(content, translation){
+    this.content = content;
+    this.translation = translation
+
+    this.processedContent = function processedContent(){
+        return this.processor(this.translation)
+    }
+}
+
+TranslatedPhrase.prototype = new Phrase()
+},{}]},{},[1]);
