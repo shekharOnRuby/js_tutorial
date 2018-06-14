354d5928dd2c25142b39cec3705094c2dd4dfda6
Shekhar Karande <shekhar.karande@gmail.com>
2018-06-14 17:30:42 +0100

    adding9.2

--- testPalindrome/main.js
+++ testPalindrome/main.js
@@ -1,11 +1,21 @@
 let Phrase = require("shk-palindrome")
 
-let string = prompt("Please enter a string for palindrome testing: ")
+function palindromeTester(){
+    let string = prompt("Please enter a string for palindrome testing: ")
 
-let phrase = new Phrase(string)
+    let phrase = new Phrase(string)
 
-if (phrase.palindrome()){
-    alert(`"${phrase.content}" is a Palindrome`)
-}else {
-    alert(`"${phrase.content}" is not a Palindrome`)
+    if (phrase.palindrome()){
+        alert(`"${phrase.content}" is a Palindrome`)
+    }else {
+        alert(`"${phrase.content}" is not a Palindrome`)
+    }
+
+  
 }
+document.addEventListener("DOMContentLoaded",function(){
+    let button = document.querySelector("#palindromeTester")
+    button.addEventListener("click",function(){
+        palindromeTester()
+    })
+})
